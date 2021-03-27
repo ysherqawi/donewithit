88238c95025f027e150f59debeb843015d15f02e
ysherqawi <ysherqawi@gmail.com>
2021-03-27 16:26:38 +0300

    Improve Welcome Screeen

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import AppButton from './app/components/AppButton';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppButton title='Login' onPress={() => console.log('Tapped')} />;
-    </View>
-  );
+  return <WelcomeScreen />;
 }
