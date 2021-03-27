e1b5505630ad01587223b17971c548af6aa2f1ed
ysherqawi <ysherqawi@gmail.com>
2021-03-27 16:03:57 +0300

    Build Button Component

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import { View } from 'react-native';
+
+import AppButton from './app/components/AppButton';
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AppButton title='Login' onPress={() => console.log('Tapped')} />;
+    </View>
+  );
 }
