709690d27d790d3c9d69392eb02ab4c6560b32b4
ysherqawi <ysherqawi@gmail.com>
2021-03-29 20:54:17 +0300

    Improve View Image Screen

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
