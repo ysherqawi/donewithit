9fcd9a75074c6b2813aedce449eb406ebfde1336
ysherqawi <ysherqawi@gmail.com>
2021-03-29 15:41:38 +0300

    Build Listing Details Screen

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+import AppText from '../components/AppText';
+import colors from '../config/colors';
+
+function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red Jacket For Sale</AppText>
+        <AppText style={styles.price}>100$</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+});
+
+export default ListingDetailsScreen;
