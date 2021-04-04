2a8f588a4e365c6acc4a852f127834604ecd554b
ysherqawi <ysherqawi@gmail.com>
2021-04-04 15:11:41 +0300

    Build Icon Component

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name='email' size={50} backgroundColor='red' iconColor='white' />
+    </Screen>
+  );
 }
