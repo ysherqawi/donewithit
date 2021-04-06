db33747efb555890c97e178a3e0a595fe43a8607
ysherqawi <ysherqawi@gmail.com>
2021-04-06 16:10:28 +0300

    Build Login Form

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+import AppTextInput from '../components/AppTextInput';
+import AppButton from '../components/AppButton';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password'),
+});
+
+function LoginScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors }) => (
+          <>
+            <AppTextInput
+              autoCapitalize='none'
+              autoCorrect={false}
+              icon='email'
+              keyboardType='email-address'
+              onChangeText={handleChange('email')}
+              placeholder='Email'
+              textContentType='emailAddress'
+            />
+            <AppText style={{ color: 'red' }}>{errors.email}</AppText>
+            <AppTextInput
+              autoCapitalize='none'
+              autoCorrect={false}
+              icon='lock'
+              keyboardType='email-address'
+              placeholder='Password'
+              secureTextEntry
+              textContentType='password'
+              onChangeText={handleChange('password')}
+            />
+            <AppText style={{ color: 'red' }}>{errors.password}</AppText>
+            <AppButton title='Login' onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 10 },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+
+export default LoginScreen;
