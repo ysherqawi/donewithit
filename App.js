f72d2d50220646b6d834a5beebda537180669c02
ysherqawi <ysherqawi@gmail.com>
2021-04-07 15:19:06 +0300

    Build Listing Edit Screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import LoginScreen from './app/screens/LoginScreen';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-  return <LoginScreen />;
+  return <ListingEditScreen />;
 }
