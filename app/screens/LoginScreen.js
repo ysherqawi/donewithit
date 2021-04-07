a9dd3d90cfaf14d4737e41b813f3d9573b1b46db
ysherqawi <ysherqawi@gmail.com>
2021-04-07 04:02:46 +0300

    Handle Touched State

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -22,29 +22,31 @@ function LoginScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors }) => (
+        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
             <AppTextInput
               autoCapitalize='none'
               autoCorrect={false}
               icon='email'
               keyboardType='email-address'
+              onBlur={() => setFieldTouched('email')}
               onChangeText={handleChange('email')}
               placeholder='Email'
               textContentType='emailAddress'
             />
-            <ErrorMessage error={errors.email} />
+            <ErrorMessage error={errors.email} visible={touched.email} />
             <AppTextInput
               autoCapitalize='none'
               autoCorrect={false}
+              onBlur={() => setFieldTouched('password')}
+              onChangeText={handleChange('password')}
               icon='lock'
               keyboardType='email-address'
               placeholder='Password'
               secureTextEntry
               textContentType='password'
-              onChangeText={handleChange('password')}
             />
-            <ErrorMessage error={errors.password} />
+            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title='Login' onPress={handleSubmit} />
           </>
         )}
