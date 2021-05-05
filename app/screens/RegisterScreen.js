085f350dd44f38f9b247c5b40a49d3773b6473ae
ysherqawi <ysherqawi@gmail.com>
2021-05-06 01:30:19 +0300

    Implement the Registration

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,49 +1,81 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import * as Yup from "yup";
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
 
-import Screen from "../components/Screen";
-import { Form, FormField, SubmitButton } from "../components/forms";
+import authApi from '../api/auth';
+import Screen from '../components/Screen';
+import {
+  ErrorMessage,
+  Form,
+  FormField,
+  SubmitButton,
+} from '../components/forms';
+import useAuth from '../auth/useAuth';
+import usersApi from '../api/users';
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
-  email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().min(4).label("Password"),
+  name: Yup.string().required().label('Name'),
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password'),
 });
 
 function RegisterScreen() {
+  const [error, setError] = useState(false);
+  const auth = useAuth();
+
+  const handleSubmit = async (userInfo) => {
+    const result = await usersApi.register(userInfo);
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError('An unexpected error occurred.');
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await authApi.login(
+      userInfo.email,
+      userInfo.password
+    );
+    auth.logIn(authToken);
+  };
+
   return (
     <Screen style={styles.container}>
       <Form
-        initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        initialValues={{ name: '', email: '', password: '' }}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage error={error} visible={error} />
+
         <FormField
           autoCorrect={false}
-          icon="account"
-          name="name"
-          placeholder="Name"
+          icon='account'
+          name='name'
+          placeholder='Name'
         />
         <FormField
-          autoCapitalize="none"
+          autoCapitalize='none'
           autoCorrect={false}
-          icon="email"
-          keyboardType="email-address"
-          name="email"
-          placeholder="Email"
-          textContentType="emailAddress"
+          icon='email'
+          keyboardType='email-address'
+          name='email'
+          placeholder='Email'
+          textContentType='emailAddress'
         />
         <FormField
-          autoCapitalize="none"
+          autoCapitalize='none'
           autoCorrect={false}
-          icon="lock"
-          name="password"
-          placeholder="Password"
+          icon='lock'
+          name='password'
+          placeholder='Password'
           secureTextEntry
-          textContentType="password"
+          textContentType='password'
         />
-        <SubmitButton title="Register" />
+        <SubmitButton title='Register' />
       </Form>
     </Screen>
   );
